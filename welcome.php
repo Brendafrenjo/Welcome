ab1ec58aef28331ea3c876407f193016bfc4fb76
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2024-04-01 22:34:04 +0300

    first commit

---
+++ welcome.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to PHP!</title>
+</head>
+<body>
+    <?php
+echo "Welcome to the world of PHP!";
+echo "<br>";
+echo "This is a PHP program running on the server!";
+echo "<br>";
+echo "Date is: ".date("d/m/Y");
+echo("<br>");
+echo("Hello World");
+?>
+</body>
+</html>
