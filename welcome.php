fb69343ff6920c119e270352588ec5cb620ea1ea
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2024-04-01 23:42:30 +0300

    fix: Added data types and variables

--- welcome.php
+++ welcome.php
@@ -6,14 +6,32 @@
     <title>Welcome to PHP!</title>
 </head>
 <body>
-    <?php
+<?php
+echo"<h1>Brenda's Site!</h1>";
+echo"<hr />";
+echo "<p>Brenda is a frontend developer and this is her site. She is attempting to start gaining skills in reading, which prove challenging, but existing at the same time</p>";
 echo "Welcome to the world of PHP!";
-echo "<br>";
+echo "<br />";
 echo "This is a PHP program running on the server!";
-echo "<br>";
-echo "Date is: ".date("d/m/Y");
-echo("<br>");
-echo("Hello World");
+echo "<br />";
+echo "Date is: ".date("d/m/Y <br />");
+echo("Hello World <br /> <br />");
+
+$characterName = "Tom";
+$characterAge = 35;
+echo("There once was a man name $characterName <br />");
+echo("He was $characterAge years old <br />");
+$characterName = "Mike";
+$characterAge = 40;
+echo("He really liked the name $characterName <br />");
+echo("But didn't like being $characterAge <br /> <br />");
+
+$phrase = "To be or not to be";
+$age = 30;
+$gpa = 3.7;
+$isMale = true;
+null;
+echo "$phrase <br />";
 ?>
 </body>
 </html>
